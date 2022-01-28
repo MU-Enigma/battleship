01bedfc604591ff9e2960308daf53ee0c619f4f7
Androbuddy <anirudhchimpidi8@gmail.com>
2022-01-28 21:30:51 +0530

    Set base path in vite config
    * Always read docs
    
    Signed-off-by: Androbuddy <anirudhchimpidi8@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
-const { resolve } = require('path')
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
-  }
+  base: '/battleship/'
 })
