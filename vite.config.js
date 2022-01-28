4ed2294d0ddc4f5239cdcaa0e4b3587cf7e6c7f6
Androbuddy <anirudhchimpidi8@gmail.com>
2022-01-28 21:01:05 +0530

    Resolve input paths for automated deploy
    Signed-off-by: Androbuddy <anirudhchimpidi8@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+const { resolve } = require('path')
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  }
 })
